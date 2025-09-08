638d4ad6589962db1d46ca5f0c8d139d52e67e88
Nidhi Goswami <goswaminidhi180@gmail.com>
2025-09-08 16:01:25 +0530

    Solution Done

--- src/Reverse_Polish_Notation.java
+++ src/Reverse_Polish_Notation.java
@@ -1,29 +1,68 @@
 import java.util.Stack;
-
+//Code Not completed
 public class Reverse_Polish_Notation {
     public static int evalRPN(String[] tokens) {
-        int ans = 0;
-        Stack<String> stack = new Stack<>();
-        int num = 0;
+        //This One is not optimal solution
+
+//        int ans = 0;
+//        Stack<String> stack = new Stack<>();
+//        for (int i = 0; i < tokens.length; i++) {
+//            String s = tokens[i];
+//            if(s.matches("-?\\d+")){
+//                stack.push(s);
+//            }else if (!stack.isEmpty()){
+//                ans = Integer.parseInt(stack.pop());
+//                int num1 = Integer.parseInt(stack.pop());
+//                switch (s){
+//                    case "+":
+//                        ans = ans+num1;
+//                        stack.push(String.valueOf(ans));
+//                        break;
+//                    case "/":
+//                        ans =  num1/ans;
+//                        stack.push(String.valueOf(ans));
+//                        break;
+//                    case "*":
+//                        ans = ans*num1;
+//                        stack.push(String.valueOf(ans));
+//                        break;
+//                    case "-":
+//                        ans = num1 - ans;
+//                        stack.push(String.valueOf(ans));
+//                        break;
+//
+//                }
+//            }
+//        }
+//        if (!stack.isEmpty()){
+//            ans = Integer.parseInt(stack.pop());
+//        }
+
+        //Another Way of Solving this
+
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < tokens.length; i++) {
-            String s = tokens[i];
-            if(s.matches("\\d+")){
-                stack.push(s);
+            if(tokens[i].equals("+")){
+                stack.push(stack.pop() + stack.pop());
+            } else if (tokens[i].equals("*")) {
+                stack.push(stack.pop() * stack.pop());
+            } else if (tokens[i].equals("-")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num2-num1);
+            }else if (tokens[i].equals("/")) {
+                int num1 = stack.pop();
+                int num2 = stack.pop();
+                stack.push(num2/num1);
             }else {
-                int exp = 0;
-                while(num !=2){
-                    exp = Integer.parseInt(stack.pop());
-                    num++;
-                }
-                stack.push(String.valueOf(exp));
+                stack.push(Integer.parseInt(tokens[i]));
             }
-            System.out.println(stack);
         }
-        return ans;
+        return stack.peek();
     }
 
     public static void main(String[] args) {
-        String[] arr = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
+        String[] arr = {"3","11","+","5","-"};
         System.out.println(evalRPN(arr));
     }
 }
