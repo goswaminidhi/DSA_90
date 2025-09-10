8432b63bdd085db22d8d38f7438193610046a9be
Nidhi Goswami <goswaminidhi180@gmail.com>
2025-09-10 14:39:07 +0530

    Solution Done

--- src/Solved/MinStack.java
+++ src/Solved/MinStack.java
@@ -1,42 +1,70 @@
 package Solved;
-//The code is completed but not Optimal yet.
+
 import java.util.Stack;
 
 public class MinStack {
-    Stack<Integer> stack ;
+    //Not optimal solution
+//    Stack<Integer> stack ;
+//
+//    public MinStack() {
+//        stack = new Stack<>();
+//    }
+//
+//    public void push(int val) {
+//        stack.push(val);
+//    }
+//
+//    public void pop() {
+//        stack.pop();
+//    }
+//
+//    public int top() {
+//        int ans = stack.peek();
+//        return ans;
+//    }
+//
+//    public int getMin() {
+//        int ans = Integer.MAX_VALUE;
+//        for (int i = 0; i < stack.size(); i++) {
+//                int elm1 = stack.get(i);
+//                ans = Math.min(ans,elm1);
+//        }
+//        return ans;
+//      }
 
+
+    //This is efficient solution
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
     public MinStack() {
         stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
     public void push(int val) {
         stack.push(val);
+        if(minStack.isEmpty()){
+            minStack.push(val);
+        } else if (minStack.peek() > stack.peek()) {
+            minStack.push(val);
+        }
     }
 
     public void pop() {
+        if(minStack.peek().equals(stack.peek())){
+            minStack.pop();
+        }
         stack.pop();
     }
 
     public int top() {
-        int ans = stack.peek();
-        return ans;
+        return stack.peek();
     }
 
     public int getMin() {
-        int ans = Integer.MAX_VALUE;
-        for (int i = 0; i < stack.size(); i++) {
-                int elm1 = stack.get(i);
-                ans = Math.min(ans,elm1);
-        }
-        return ans;
+        return minStack.peek();
     }
 
     public static void main(String[] args) {
-        int val = 0;
-        MinStack obj = new MinStack();
-        obj.push(val);
-        obj.pop();
-        int param_3 = obj.top();
-        int param_4 = obj.getMin();
     }
 }
