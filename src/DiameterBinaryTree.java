a37fb72c923877db62d4ea58f587c9bcf7d41f2e
Nidhi Goswami <goswaminidhi180@gmail.com>
2025-09-17 18:35:14 +0530

    Solution Done

---
+++ src/DiameterBinaryTree.java
@@ -0,0 +1,30 @@
+public class DiameterBinaryTree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  int ans = 0;
+  public int diameter(TreeNode node){
+      if(node == null){
+          return 0;
+      }
+      int rh = diameter(node.right);
+      int lh = diameter(node.left);
+      ans = Math.max(ans,lh+rh);
+
+      return 1+Math.max(lh,rh);
+
+  }
+    public int diameterOfBinaryTree(TreeNode root) {
+      diameter(root);
+       return ans;
+    }
+}
