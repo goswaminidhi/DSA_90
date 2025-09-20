61bf38c27776f217bb7cd7ac07465a9498a40c7c
Nidhi Goswami <goswaminidhi180@gmail.com>
2025-09-20 12:26:26 +0530

    Solution Done

---
+++ src/Solved/ValidateBinarySearchTree.java
@@ -0,0 +1,28 @@
+package Solved;
+
+public class ValidateBinarySearchTree {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+  public boolean valid(TreeNode node,long minimum,long maximum){
+      if (node == null) {
+          return true;
+      }
+      if(!(node.val > minimum && node.val < maximum)){
+          return false;
+      }
+      return valid(node.left , minimum ,node.val) && valid(node.right,node.val,maximum);
+  }
+    public boolean isValidBST(TreeNode root) {
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+}
