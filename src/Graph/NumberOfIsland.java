de8208148018840f5b63175d489c2ff9a80ae67f
Nidhi Goswami <goswaminidhi180@gmail.com>
2025-12-13 07:22:00 +0530

    Solution Done

---
+++ src/Graph/NumberOfIsland.java
@@ -0,0 +1,43 @@
+package Graph;
+
+public class NumberOfIsland {
+    public static void dfs(char[][] grid,int i,int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
+            return ;
+        }
+
+        grid[i][j] = '0'; //Marking land as visited
+
+        //Now we are exploring in all four direction
+        dfs(grid ,i+1 ,j);
+        dfs(grid ,i-1 ,j);
+        dfs(grid ,i ,j+1);
+        dfs(grid ,i ,j-1);
+
+    }
+    public static int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int output = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == '1'){
+                    dfs(grid,i,j);
+                    output++;
+                }
+            }
+        }
+
+        return output;
+    }
+    public static void main(String[] args) {
+        char[][] grid  = {
+                {'1','1','1','1','0'},
+                {'1','1','0','1','0'},
+                {'1','1','0','0','0'},
+                {'0','0','0','0','0'}};
+        System.out.println(numIslands(grid));
+    }
+}
