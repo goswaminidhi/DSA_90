c7efe3137a4ec32479657ec40e2124bcd3da19e9
Nidhi Goswami <goswaminidhi180@gmail.com>
2026-01-01 10:35:13 +0530

    Solution done

--- src/DSA_Course/a4_Searching_Sorting/a3_BubbleSort.java
+++ src/DSA_Course/a4_Searching_Sorting/a3_BubbleSort.java
@@ -8,16 +8,27 @@ public class a3_BubbleSort {
     }
 
     public static void bubbleSort(int[] nums) {
-        int n = nums.length - 1;
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        int n = nums.length ;
+        for (int i = 0; i < n-1; i++) {//-1 because last element will automatically come to its place so no need to
+            // move it
+            boolean isSwapped = false;
+            for (int j = 1; j < n-1-i; j++) {
                 if (nums[j - 1] > nums[j]) {
                     int temp = nums[j-1];
                     nums[j - 1] = nums[j];
                     nums[j] = temp;
+                    isSwapped = true;
+                }
+                if(!isSwapped){
+                    break;
                 }
             }
         }
+
+        /*
+        If there is an iteration in which there in no swapping that means our array is already soreted than we will
+        stop all the other iterations
+         */
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
---
+++ src/DSA_Course/a4_Searching_Sorting/a4_SelectionSort.java
@@ -0,0 +1,50 @@
+package DSA_Course.a4_Searching_Sorting;
+
+import java.util.Arrays;
+
+public class a4_SelectionSort {
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int actualMin = minIndex(i, arr);
+            int temp = arr[i];
+            arr[i] = arr[actualMin];
+            arr[actualMin] = temp;
+        }
+    }
+
+    private static int minIndex(int start, int[] arr) {
+        int minInd = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = start; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                minInd = i;
+            }
+        }
+        return minInd;
+    }
+
+    public static void selection(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 7, 12, 1, 6, 56, 9};
+        System.out.println(Arrays.toString(arr));
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
