0cb8761f95a3fddc000dd8705544af94a43bb426
Nidhi Goswami <goswaminidhi180@gmail.com>
2026-01-22 09:17:31 +0530

    Binary Search Solution Done

---
+++ src/DSA_Course/BinarySearch/a1_Sqrt.java
@@ -0,0 +1,27 @@
+package DSA_Course.BinarySearch;
+
+public class a1_Sqrt {
+    public static int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 2;
+        int right = x / 2;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            long square = (long) mid * mid;
+            if (x == square) {
+                return mid;
+            } else if (x > square) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(4));
+        System.out.println(mySqrt(8));
+    }
+}
