a37534a3bf842916edfef85bd62126c1960d2d84
Nidhi Goswami <goswaminidhi180@gmail.com>
2026-02-09 12:32:47 +0530

    Binary Search Solution Done

--- src/DSA_Course/BinarySearch/a1_Sqrt.java
+++ src/DSA_Course/BinarySearch/a1_Sqrt.java
@@ -7,7 +7,8 @@ public class a1_Sqrt {
         int left = 2;
         int right = x / 2;
         while (left <= right) {
-            int mid = (left + right) / 2;
+//            int mid = (left + right) / 2;
+            int mid = left+((right-left)/2);
             long square = (long) mid * mid;
             if (x == square) {
                 return mid;
