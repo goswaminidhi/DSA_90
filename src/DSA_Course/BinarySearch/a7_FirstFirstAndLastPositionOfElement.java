f21cac8f7b0c391d8d34675f13389c9a84ffa4dc
Nidhi Goswami <goswaminidhi180@gmail.com>
2026-02-09 12:31:56 +0530

    Binary Search Solution Done

---
+++ src/DSA_Course/BinarySearch/a7_FirstFirstAndLastPositionOfElement.java
@@ -0,0 +1,43 @@
+package DSA_Course.BinarySearch;
+
+public class a7_FirstFirstAndLastPositionOfElement {
+    public static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        if(nums.length == 0){
+            return ans;
+        }
+        int left1 = 0;
+        int right1 = nums.length-1;
+        while (left1 < right1){
+            int mid = left1 + (right1 - left1)/2;
+            if(nums[mid] < target){
+                left1 = mid+1;
+            }else {
+                right1 = mid;
+            }
+        }
+        if(nums[left1] == target){
+            ans[0] = left1;
+        }
+        int left2 = 0;
+        int right2 = nums.length-1;
+        while (left2 < right2){
+            int mid = left2 + (right2 - left2 + 1)/2;
+            if(nums[mid] > target){
+                right2 = mid - 1;
+            }else {
+                left2 = mid;
+            }
+        }
+        if(nums[left2] == target){
+            ans[1] = left2;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        System.out.println(searchRange(new int[]{5,7,7,8,8,10},8));
+        System.out.println(searchRange(new int[]{5,7,7,8,8,10},6));
+        System.out.println(searchRange(new int[]{},0));
+
+    }
+}
